835941606418702acfa93d763ad0af2cbe2f2402
tainalopes <vanillaros@gmail.com>
2025-04-09 23:21:53 -0300

    STK-01: add mensagem de validação de campos nulos e formato do CEP.

---
+++ src/main/java/com/lopes/beckers_delivery_api/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.lopes.beckers_delivery_api.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult().getFieldError().getDefaultMessage();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), errorMessage));
+    }
+}
